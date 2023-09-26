c18ec8df4ca9c6f928cf6c83acca300e9782e1fd
IvanSitnikov1 <iw.sitnikoff@yandex.ru>
2023-09-26 08:45:46 +0300

    переименовано представление index на employee_tree, рефакторинг

--- test_task/main/views.py
+++ test_task/main/views.py
@@ -4,33 +4,36 @@ from django.views.generic import ListView
 from .models import *
 
 
-def employee(request):
-    employees = Employee.objects.all()
-    cats = {}
-    for i in employees:
-        if i.parent_id in cats:
-            cats[i.parent_id].append(i)
+def employee_tree(request):
+    def build_tree(employee_dict, parent_id, lvl):
+        lvl -= 1
+        if parent_id in employee_dict:
+            tree = '<ul>'
+            for i in employee_dict[parent_id]:
+                if lvl == 0:
+                    tree += f'<li style="display: none">{i.name} - {i.position}'
+                else:
+                    tree += f'<li>{i.name} - {i.position}'
+                tree += build_tree(cats, i.id, lvl)
+                tree += '</li>'
+            tree += '</ul>'
         else:
-            cats[i.parent_id] = [i]
-    tree = build_tree(cats, 1, 3)
+            return ''
+        return tree
+
+    employees = Employee.objects.all()
+    employee_dict = {}
+    for i in employees:
+        if i.parent_id in employee_dict:
+            employee_dict[i.parent_id].append(i)
+        else:
+            employee_dict[i.parent_id] = [i]
+    tree = build_tree(employee_dict, 1, 3)
     boss = Employee.objects.get(parent_id__isnull=True)
     context = {'boss': boss, 'tree': tree}
-    return render(request, 'main/base.html', context)
+    return render(request, 'main/employee_tree.html', context)
 
 
-def build_tree(cats, parent_id, lvl):
-    lvl -= 1
-    if parent_id in cats:
-        tree = '<ul>'
-        for i in cats[parent_id]:
-            if lvl == 0:
-                tree += f'<li style="display: none">{i.name} - {i.position}'
-            else:
-                tree += f'<li>{i.name} - {i.position}'
-            tree += build_tree(cats, i.id, lvl)
-            tree += '</li>'
-        tree += '</ul>'
-    else:
-        return ''
 
-    return tree
+
+
